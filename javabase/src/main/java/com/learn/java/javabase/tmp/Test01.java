e5b77a27d5eef1a9283bf5462d7cb19114390cf6
fangjuncai <997479263@qq.com>
2020-03-25 21:17:08 +0800

    Javabase test

---
+++ javabase/src/main/java/com/learn/java/javabase/tmp/Test01.java
@@ -0,0 +1,12 @@
+package com.learn.java.javabase.tmp;
+
+public class Test01 {
+    public static void main(String[] args) {
+        System.out.println((long)Integer.valueOf(-1));
+    }
+    private static void testSubstring(){
+
+        String s ="012345";
+        System.out.println(s.substring(1,4));
+    }
+}
